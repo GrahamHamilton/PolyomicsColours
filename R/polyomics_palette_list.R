0b66a0ae40d458a364ca09e02da0f9ef85b995ab
Graham Hamilton <graham.hamilton@glasgow.ac.uk>
2022-07-05 15:40:37 +0100

    Added more colours and palettes

--- R/polyomics_palette_list.R
+++ R/polyomics_palette_list.R
@@ -12,12 +12,12 @@
 #'
 polyomics_palettes_list <- function (...){
   polyomics_palettes <- list(
-    `main`  = polyomics_cols("University Blue", "Leaf", "Lavender"),
-    `cool`  = polyomics_cols("University Blue", "Leaf"),
-    `hot`   = polyomics_cols("Sunshine", "Pumpkin", "Pillarbox"),
-    `mixed` = polyomics_cols("Lavender","University Blue", "Sunshine", "Pumpkin", "Pillarbox","Thistle"),
-    `all` = polyomics_cols("University Blue","Burgundy","Turquiose","Cobalt","Lavender","Leaf","Moss","Pillarbox","Rust","Pumpkin","Sunshine","Sky Blue","Slate","Thistle","Grey","Light Grey","Dark Grey","Black"),
-    `colour_blind` = polyomics_cols("Lavender","Sunshine"),
+    `main`  = polyomics_cols("University Blue","Burgundy","Cobalt","Lavender","Leaf","Moss","Pillarbox","Rust","Sandstone","Sky Blue","Slate","Thistle"),
+    `secondary` = polyomics_cols("Turquiose","Rose","Pumpkin","Sunshine","Mocha"),
+    `cool`  = polyomics_cols("University Blue", "Lavender", "Leaf", "Moss"),
+    `hot`   = polyomics_cols("Sunshine", "Pumpkin", "Rust", "Pillarbox"),
+    `mixed` = polyomics_cols("Lavender","University Blue", "Sunshine", "Pumpkin", "Pillarbox","Thistle","Mocha"),
+    `all` = polyomics_cols("University Blue","Burgundy","Cobalt","Lavender","Leaf","Moss","Pillarbox","Rust","Sandstone","Sky Blue","Slate","Thistle","Turquiose","Rose","Pumpkin","Sunshine","Mocha","Grey","Light Grey","Dark Grey","Black"),
     `grey`  = polyomics_cols("Light Grey", "Dark Grey", "Slate")
   )
   return(polyomics_palettes)
